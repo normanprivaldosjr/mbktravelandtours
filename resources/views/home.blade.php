715e8bc9a3de3b05f502758281d829d1c5ebdad4
Danilo Cielos Jr <dcielosjr@gmail.com>
2017-08-13 19:02:25 -0600

    Uhm trying to commit?

---
+++ resources/views/home.blade.php
@@ -0,0 +1,15 @@
+<html>
+<head>
+    <title>Home Page</title>
+    <link rel="stylesheet" href="/css/app.css">
+    <script src="/js/app.js"></script>
+</head>
+<body>
+<div class="container">
+    <div class="content">
+        <div class="title">Home Page</div>
+        <div class="quote">Our Home page!</div>
+    </div>
+</div>
+</body>
+</html>
